8119f150226982fb91e2ff5c674eb85c47a7e351
tylermachado <3586466+tylermachado@users.noreply.github.com>
2026-02-04 10:40:37 -0500

    add event component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,27 @@
+<script>
+  import { onMount } from 'svelte';
+  import Event from '$lib/Event.svelte';
+
+  let events = [];
+
+  onMount(async () => {
+    const response = await fetch('/data/schedule.json');
+    const data = await response.json();
+    events = data.filter(event => event.date === 'February 4, 2026');
+  });
+</script>
+
 <section class="prose">
-	<h1>Winter Olympics Watch Guide</h1>
-	<p>This demo shows a simple schedule and medal tracker. Use the header to navigate.</p>
+  <h1>Winter Olympics Watch Guide</h1>
+  <p>This demo shows a simple schedule and medal tracker. Use the header to navigate.</p>
+
+  {#each events as event}
+    <Event 
+      startTime={event.start_time} 
+      sport={event.sport} 
+      event={event.event} 
+      isLive={event.is_live} 
+      isReplay={event.is_replay} 
+      broadcaster={event.broadcaster} />
+  {/each}
 </section>
